81e66c9b0d76e0a54f5c6d3e000727d9da1849fb
sojib-bd <sojibrahmatuzzaman@gmail.com>
2020-04-13 12:04:44 +0600

    make the reducer

---
+++ src/redux/actions/reducers/cartReducers.js
@@ -0,0 +1,23 @@
+import { ADD_TO_CART, REMOVE_FROM_CART } from "../cartActions";
+
+const initialState = {
+    cart: []
+}
+
+const cartReducers = (state, action) => {
+    switch (action.type) {
+        case ADD_TO_CART:
+            const newCart = [...state.cart, action.id];// new id is added to the empty cart
+            return { cart: newCart }
+
+        case REMOVE_FROM_CART:
+            const id = action.id;
+            const remainingCart = state.cart.filter(item => item !== id);//remove the id from the cart
+            return { cart: remainingCart }
+
+        default:
+            return state
+    }
+}
+
+export default cartReducers;
